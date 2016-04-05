dca4ba86af2f024a1fe387bd944e9e4a8ba8e0b2
Oleksandr <avpavlov96@gmail.com>
2016-04-05 15:39:32 +0300

    servlet-mapping

--- src/ru/emv/web/servlets/Redirect.java
+++ src/ru/emv/web/servlets/Redirect.java
@@ -1,10 +1,22 @@
 package ru.emv.web.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by ALEX on 05.04.2016.
  */
 public class Redirect extends HttpServlet {
+    private static int count = 0;
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        count++;
+
+        request.setAttribute("curent_count", count);
+        request.getRequestDispatcher("index.jsp").forward(request,response);
+    }
 }
